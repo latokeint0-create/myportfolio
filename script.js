9f040f365c273d2a5a53970cb7382b9539887a1a
latokeint0-create <latokeint0@gmail.com>
2025-11-14 22:36:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function readMore() {
+  // Navigate to the 'Life as Student' page
+  // Use a relative path so it works when hosted under the same folder
+  window.location.href = 'student.html';
+}
